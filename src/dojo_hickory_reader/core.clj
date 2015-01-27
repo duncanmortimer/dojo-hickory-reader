acf6f4c40fe28b57c8157b480baf10525edc9368
Duncan Mortimer <dmortime@thoughtworks.com>
2015-01-27 20:30:36 +0000

    Count html elements

--- src/dojo_hickory_reader/core.clj
+++ src/dojo_hickory_reader/core.clj
@@ -1,8 +1,39 @@
 (ns dojo-hickory-reader.core
   (:require [clj-http.lite.client :as client]
-            [hickory.core :as hickory]))
+            [hickory.core :as hickory]
+            [hickory.select :as s]))
 
 (defn read-url [url]
   (let [response (client/get url)]
     (when (= (response :status) 200)
       (response :body))))
+
+(defn parse-to-hickory [url]
+  (-> url
+      read-url
+      hickory/parse
+      hickory/as-hickory))
+
+(defn select-head [hickory-document]
+  (s/select (s/tag :head) hickory-document))
+
+(defn select-body [hickory-document]
+  (s/select (s/tag :body) hickory-document))
+
+
+
+(defn count-elements [hickory-document]
+  (->> hickory-document
+       (tree-seq :content :content)
+       (filter #(= (% :type) :element))
+       count))
+
+
+
+
+
+
+
+
+
+
